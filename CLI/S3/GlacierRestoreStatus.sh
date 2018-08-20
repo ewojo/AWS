4c1305feb5cf499e684487f19952624a7c89bbc0
Eric Wojtowicz <ewojo@users.noreply.github.com>
2018-08-20 15:40:09 -0400

    Update GlacierRestoreStatus.sh

--- CLI/S3/GlacierRestoreStatus.sh
+++ CLI/S3/GlacierRestoreStatus.sh
@@ -1,21 +1,23 @@
-#!bin/bash
+ #!bin/bash
+ 
 #set the bucket name
-bucket=bucketname
+bucket=BUCKETNAME
+ 
 #set the storage class
 class=GLACIER
+ 
 #loop though the objects and find something
 IFS=$(echo -en "\n\b\t")
 SAVEIFS=$IFS
-for a in $(aws s3api list-object-versions --bucket $bucket --output json --query 'Versions[?StorageClass==`'$class'`].Key'| jq -r .[]);
-do
-restore=$(aws s3api head-object --bucket $bucket --key $a --query Restore --output text);
-if [ $restore == "None" ];
-then
-echo $a "Has not been Restored"
-elif [[ $restore == *true* ]];
-then
-echo $a "Is being Restored"
-else
-echo "Object " $a "is Restored for: " $restore
-fi
+for a in $(aws s3api  list-object-versions --bucket $bucket  --output json --query 'Versions[?StorageClass==`'$class'`].Key'| jq -r .[]);
+        do
+        # Run a head object api call to get the restore status of the Glacier object
+        restore=$(aws s3api head-object --bucket $bucket --key $a --query Restore --output text);
+ 
+        # If there is no ongoing restore, and expiry date, then the object is not restored, and not in restoration progress
+        if [ $restore == "None" ];
+        then
+                #echo $a "Has not been Restored"
+                echo $a
+        fi
 done
